a8d6006db59bfa818c911e8b6874cc38cf46f4d2
j-nac <49133530+j-nac@users.noreply.github.com>
2020-03-18 16:44:05 -0700

    Add files via upload

---
+++ base.js
@@ -0,0 +1,58 @@
+var activePage = document.getElementById("idpage").textContent;
+
+
+function addFooter() {
+  if (activePage == "Welcome") {
+  document.getElementById("stdfooter").innerHTML = "\
+<table>\
+  <tr>\
+    <th><a href=\"html/about.html\">About</a></th>\
+    <th><a href=\"html/blog.html\">Blog</a></th>\
+    <th><a href=\"html/stuff.html\">Stuff</a></th>\
+  </tr>\
+  <tr>\
+    <td>A Link 1</td>\
+    <td>B Link 1</td>\
+    <td>S Link 1</td>\
+  </tr>\
+  <tr>\
+    <td>A Link 2</td>\
+    <td>B Link 2</td>\
+    <td>S Link 2</td>\
+  </tr>\
+  <tr>\
+    <td>A Link 3</td>\
+    <td>B Link 3</td>\
+    <td>S Link 3</td>\
+  </tr>\
+</table>";
+  } else {
+     document.getElementById("stdfooter").innerHTML = "\
+<table>\
+  <tr>\
+    <th><a href=\"about.html\">About</a></th>\
+    <th><a href=\"blog.html\">Blog</a></th>\
+    <th><a href=\"stuff.html\">Stuff</a></th>\
+  </tr>\
+  <tr>\
+    <td>A Link 1</td>\
+    <td>B Link 1</td>\
+    <td>S Link 1</td>\
+  </tr>\
+  <tr>\
+    <td>A Link 2</td>\
+    <td>B Link 2</td>\
+    <td>S Link 2</td>\
+  </tr>\
+  <tr>\
+    <td>A Link 3</td>\
+    <td>B Link 3</td>\
+    <td>S Link 3</td>\
+  </tr>\
+</table>"; 
+  }
+}
+
+window.onload = function onLoad() {
+  addFooter();
+}
